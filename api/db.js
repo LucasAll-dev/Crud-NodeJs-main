6efa397fc8aa699cd9f854a55a7048449d9c6f0e
LucasAll-dev <jl012593@gmail.com>
2025-03-09 20:19:17 -0300

    atualizacao de components

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "0305829",
+    password: "willdev123",
     database: "crud"
 })
